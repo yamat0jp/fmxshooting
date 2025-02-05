7c20fe71f782bd47e24b0aff5ca3ed15fa596d16
yamat0jp <yamat0jp@yahoo.co.jp>
2025-02-05 19:00:31 +0900

    module update
    モジュールのメソッドを強化

--- Unit1.pas
+++ Unit1.pas
@@ -47,15 +47,25 @@ type
     Panel1: TPanel;
     SpeedButton1: TSpeedButton;
     PaintBox1: TPaintBox;
+    ListBox1: TListBox;
+    Label1: TLabel;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure PythonModule1Events0Execute(Sender: TObject;
       PSelf, Args: PPyObject; var Result: PPyObject);
+    procedure PythonModule1Events2Execute(Sender: TObject;
+      PSelf, Args: PPyObject; var Result: PPyObject);
+    procedure SpeedButton1Click(Sender: TObject);
+    procedure PythonModule1Events1Execute(Sender: TObject;
+      PSelf, Args: PPyObject; var Result: PPyObject);
+    procedure ListBox1Change(Sender: TObject);
   private
     { private éŒ¾ }
     enemy: TCharObj;
     time: TTime;
+    motion: Boolean;
     procedure AppOnIdle(Sender: TObject; var Done: Boolean);
+    procedure touroku;
   public
     { public éŒ¾ }
   end;
@@ -66,6 +76,8 @@ var
 implementation
 
 {$R *.fmx}
+
+uses Math;
 
 procedure TForm1.AppOnIdle(Sender: TObject; var Done: Boolean);
 const
@@ -78,12 +90,13 @@ begin
   if fps * new * 24 * 3600 > 1 then
   begin
     PaintBox1.Canvas.BeginScene;
-//    PaintBox1.Canvas.Clear(TAlphaColors.White);
-    if SpeedButton1.IsPressed then
-      PythonEngine1.ExecStrings(Memo1.Lines);
+    PaintBox1.Canvas.Clear(TAlphaColors.White);
+    if motion then
+      enemy.shooting;
     enemy.beams;
-    time := time + new;
     PaintBox1.Canvas.EndScene;
+    Panel1.Repaint;
+    time := time + new;
   end;
   enemy.count := enemy.count + 1;
 end;
@@ -95,6 +108,7 @@ begin
   enemy.top := 50;
   time := Now;
   PaintBox1.Canvas.Fill.Color := TAlphaColors.Blue;
+  touroku;
   Application.OnIdle := AppOnIdle;
 end;
 
@@ -103,10 +117,51 @@ begin
   enemy.Free;
 end;
 
+procedure TForm1.ListBox1Change(Sender: TObject);
+begin
+  with PythonModule1.Events do
+    for var i := 0 to count - 1 do
+      if Items[i].Name = ListBox1.Items[ListBox1.ItemIndex] then
+        Label1.Text := Items[i].DocString.Text;
+end;
+
 procedure TForm1.PythonModule1Events0Execute(Sender: TObject;
   PSelf, Args: PPyObject; var Result: PPyObject);
 begin
-  enemy.shooting;
+  motion := true;
+end;
+
+procedure TForm1.PythonModule1Events1Execute(Sender: TObject;
+  PSelf, Args: PPyObject; var Result: PPyObject);
+begin
+  motion := false;
+end;
+
+procedure TForm1.PythonModule1Events2Execute(Sender: TObject;
+  PSelf, Args: PPyObject; var Result: PPyObject);
+var
+  i: integer;
+  a, b, t: Single;
+begin
+  if PythonEngine1.PyArg_ParseTuple(Args, 'i', @i) > 0 then
+  begin
+    a := enemy.speedx;
+    b := enemy.speedy;
+    t := DegToRad(i);
+    enemy.speedx := cos(t) * a - sin(t) * b;
+    enemy.speedy := sin(t) * a + cos(t) * b;
+  end;
+end;
+
+procedure TForm1.SpeedButton1Click(Sender: TObject);
+begin
+  PythonEngine1.ExecStrings(Memo1.Lines);
+end;
+
+procedure TForm1.touroku;
+begin
+  for var i := 0 to PythonModule1.Events.count - 1 do
+    ListBox1.Items.Add(PythonModule1.Events.Items[i].Name);
 end;
 
 { TCharObj }
@@ -148,8 +203,8 @@ begin
   begin
     left := Self.left;
     top := Self.top;
-    speedx := a * cos(Self.theta);
-    speedy := a * sin(Self.theta);
+    speedx := Self.speedx + a * cos(Self.theta);
+    speedy := Self.speedy + a * sin(Self.theta);
   end;
   Self.theta := Self.theta + theta;
 end;
